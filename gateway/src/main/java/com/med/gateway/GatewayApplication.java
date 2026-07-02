33951a7acfea10f9e338a84718e106ed1dcedfda
Mayyaamy <2140246518@qq.com>
2026-07-02 11:53:49 +0800

    阶段8：课程设计结题收尾，新增全套设计文档，所有分布式考核点全部闭环，53个接口达标，版本归档完成

--- gateway/src/main/java/com/med/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/med/gateway/GatewayApplication.java
@@ -1,4 +1,14 @@
 package com.med.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+// 开启Nacos服务注册，网关自己也注册进Nacos
+@EnableDiscoveryClient
 public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
