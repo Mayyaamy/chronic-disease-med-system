9312ba5b1a23449a34c04b71507870d8c85f5f8b
Mayyaamy <2140246518@qq.com>
2026-07-02 11:02:14 +0800

    阶段3：auth-service接入Nacos注册+配置中心，远端存放数据库Redis配置，MyBatis-Plus+Druid适配chronic-med-db，开启Feign与服务发现

--- auth-service/src/main/java/com/med/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/med/auth/AuthServiceApplication.java
@@ -1,17 +1,17 @@
 package com.med.auth;
 
-import com.med.auth.config.JwtProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@MapperScan("com.med.auth.mapper")
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableDiscoveryClient // 开启Nacos服务注册与发现
+@EnableFeignClients    // 开启OpenFeign远程调用，预留多服务调用能力
+@MapperScan("com.med.auth.mapper") // 扫描Mapper接口
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
-
--- auth-service/src/main/java/com/med/auth/config/MybatisPlusConfig.java
+++ auth-service/src/main/java/com/med/auth/config/MybatisPlusConfig.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MybatisPlusConfig {
+public class MyBatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return interceptor;
     }
